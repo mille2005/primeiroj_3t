9d1e7c0da23e69e227a37c51f369001003ff397e
mille2005/primeiroj_2t <106184923+mille2005@users.noreply.github.com>
2022-10-05 10:44:09 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -2,8 +2,9 @@
 // alert("Alerta: Alô mundo...");
 let tabuada = 2;
 
-let serie= 1;
+
 function meChame(){
+    document.write("Tabuada do " + tabuada + "<br>");
     document.write(tabuada + " x 1 = " + (tabuada*1) +"<br>");
     document.write(tabuada + " x 2 = " + (tabuada*2) +"<br>");
     document.write(tabuada + " x 3 = " + (tabuada*3) +"<br>");
@@ -13,24 +14,23 @@ function meChame(){
     document.write(tabuada + " x 7 = " + (tabuada*7) +"<br>");
     document.write(tabuada + " x 8 = " + (tabuada*8) +"<br>");
     document.write(tabuada + " x 9 = " + (tabuada*9) +"<br>");
-    document.write(tabuada + " x 10 = " + (tabuada*10) +"<br>");
-    
+    document.write(tabuada + " x 10 = " + (tabuada*10) +"<br>");    
 }
+
 
 function minhaTabuada(){
     for(var i=1; i <= 10; i++){
+        
        for(var j = 1; j <=10; j++) {
         document.write(i + " x " + j + " = " + (i*j) +"<br>");   
        }
        
        document.write("<br><br>");
-  
     }
 }
 
 function quadrado(){
     for(var i = 2; i < 21; i++){
-        document.write("O quadrado de " + i + " é " + (i*i) + "<br>")
-
+        document.write("O quadrado de " + i + " é " + (i*i) + "<br>");
     }
 }
